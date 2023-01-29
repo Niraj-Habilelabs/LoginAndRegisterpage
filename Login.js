ff712223387b80e27e85bcad053f944ee1fc6e51
Niraj Rai <niraj.rai@habilelabs.io>
2023-01-29 22:16:54 +0530

    File uploded

---
+++ Login.js
@@ -0,0 +1,50 @@
+
+  alert()
+  function toggle(){
+    let password = document.querySelector(".login_password");
+    let eye_icon = document.querySelector("#eye");
+    if(password.type == "text"){
+      password.setAttribute("type","password");
+      eye_icon.className="fa fa-eye";
+    }
+    else {
+      password.setAttribute("type","text");
+      eye_icon.className="fa fa-eye-slash";
+
+    }
+  }
+
+let notify = document.querySelector(".notify");
+function getData(event){
+    event.preventDefault();
+    let user = document.querySelector(".login_user");
+    let pass = document.querySelector(".login_password");
+    if(localStorage.getItem(user.value)){
+      /** user found */
+      let data = JSON.parse(localStorage.getItem(user.value));
+      if(pass.value == data['password']){
+        /** password match */
+        notify.textContent="login success !"
+        notify.className="notify green";
+
+      }
+      else{
+        /** password not match */
+        notify.textContent="password not match !"
+        notify.className="notify red";
+
+
+      }
+
+
+    }
+    else{
+      /** user not found */
+      notify.textContent="user not found !"
+      notify.className="notify red";
+
+    }
+
+
+   
+}
